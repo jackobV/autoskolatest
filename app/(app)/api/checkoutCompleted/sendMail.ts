6e2f1c132c970425842702a74407f9c763e9043a
Jakub Zaloha <zalohjak@fit.cvut.cz>
2024-07-15 12:21:29 +0200

    lots

--- app/(app)/api/checkoutCompleted/sendMail.ts
+++ app/(app)/api/checkoutCompleted/sendMail.ts
@@ -1,5 +1,4 @@
-var postmark = require("postmark");
-// @ts-ignore
+const postmark = require("postmark");
 const postmarkClient = new postmark.ServerClient(process.env.POSTMARK_SERVER_TOKEN);
 
 // @ts-ignore
@@ -16,10 +15,22 @@ export default async function sendEmail({ to, from, subject, message }) {
         console.log('Email sent successfully!');
         return result;
     } catch (error) {
-        console.error('Error sending email:', error);
-        throw error;
+        // @ts-ignore
+        if (error.code === 'EMFILE') {
+            console.error('Too many open files:', error);
+            // Implement a retry mechanism with delay
+            await new Promise(resolve => setTimeout(resolve, 1000));
+            return sendEmail({ to, from, subject, message });
+        } else { // @ts-ignore
+            if (error.code === 'ENETUNREACH') {
+                        console.error('Network unreachable:', error);
+                        // Retry or handle network error appropriately
+                        await new Promise(resolve => setTimeout(resolve, 1000));
+                        return sendEmail({ to, from, subject, message });
+                    } else {
+                        console.error('Error sending email:', error);
+                        throw error;
+                    }
+        }
     }
 }
-
-		
-    
